ad41883e440f22a0ffdd933b24d84ab8ba70b106
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-13 09:26:47 +1100

    installed cookie-parser, body-parser, created crypto.js file with functions and request handlers - need to work on dividind functions to files

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -1,25 +1,39 @@
-fetch(`/api/login/${id}`, {
+GET http://localhost:8080/users
+
+POST http://localhost:8080/users
+Content-Type: 'application/json
+
+{
+    "name":"Bryan"
+    "password": password
+}
     method: 'PUT',
     headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+      Accept: 'application/json',
+      
     },
+    */
 
-
+//Check if the user is authenticated:
+//middleware function named CheckAuth used on every route that needs the user to be authenticated:
     function checkAuth(req, res, next) {
-    if(!req.session.user_id) {
-    res.send('You are not authorized to view this page');
-} else {
-    next();
-}
+        if (!req.session.user_id) {
+          res.send('You are not authorized to view this page');
+        } else {
+          next();
+        }
       }
 
-app.post('/login', function (req, res) {
-    var post = req.body;
-    if (post.user === 'username' && post.password === 'usernamespassword') {
-        req.session.user_id = user_name_id_here;
-        res.redirect('/');
-    } else {
-        res.send('Bad user/pass');
-    }
-});
+      app.get('/', checkAuth, function (req, res) {
+        res.send('if you are viewing this page it means you are logged in');
+      });
+
+      app.post('/login', function (req, res) {
+        var post = req.body;
+        if (post.user === 'username' && post.password === 'usernamespassword') {
+          req.session.user_id = user_name_id_here;
+          res.redirect('/');
+        } else {
+          res.send('Bad user/pass');
+        }
+      });
--- server.js
+++ server.js
@@ -1,56 +1,35 @@
 const express = require("express");
-//const exphbs = require("express-handlebars");
-//const htmlRouter = require("./routes/html-routes.js");
+const exphbs = require("express-handlebars");
+const htmlRouter = require("./routes/html-routes.js");
 //const apiRouter = require("./routes/api-routes.js")
+
+const crypto = require("./config/crypto");
 
 // Sets up the Express App
 const app = express();
-//const PORT = process.env.PORT || 8080;
-
-app.use(express.json());
-
-const users = [];
-
-app.get("/users", (req, res) => {
-  res.json(users);
-});
-
-app.post("/users", (req, res) => {
-  const user = { name: req.body.name, password: req.body.password };
-  users.push(user);
-  res.status(201).send();
-});
-
-app.post("/users/login", (req, res) => {
-  const user = users.find(user => user.name = req.body.name);
-  if (user === null) {
-    return res.status(400).send("Cannot find user");
-  }
-});
-
-app.listen(3000);
+const PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-//const db = require("./models");
+const db = require("./models");
 
 // Sets up the Express app to handle data parsing
 //app.use(express.urlencoded({ extended: true }));
-//app.use(express.json());
+app.use(express.json());
 
 // Static directory
-//app.use(express.static("public"));
+app.use(express.static("public"));
 
-//app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Invoke routes
 // htmlRouter(app);
 // apiRouter(app);
-//app.use(htmlRouter);
+app.use(htmlRouter);
 // app.use(apiRouter);
 
 // Syncing our sequelize models and then starting our Express app
 // !! REMOVE "{ force: true }" @ deployment !!
-//db.sequelize.sync({ force: true }).then(() => {
-  //app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
-//});
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+});

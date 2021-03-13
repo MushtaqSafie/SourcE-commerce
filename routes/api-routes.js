6b43a9015a45027123901edce192d7ebcbb5f71b
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-13 11:05:22 +1100

    updated app to route. for routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,14 +1,85 @@
 const db = require("../models");
 const crypto = require("crypto")
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.render('main');
 });
 
-app.get('/index', (req, res) => {
+router.get('/index', (req, res) => {
     res.render('index')
 });
 
-app.post('/index', (req, res) => {
+router.post('/index', (req, res) => {
     const { email, password } = req.body;
     const hashedPassword = getHashedPassword(password);
+    const user = users.find(u => {
+        return u.email === email && hashedPassword === u.password
+    });
+
+    if (user) {
+        const authToken = generateAuthToken();
+
+        authTokens[authToken] = email;
+
+        res.cookie('AuthToken', authToken);
+        res.redirect('/protected')
+        return;
+    } else {
+        res.render('/index', {
+            messageClass: 'Invalid username or password',
+            messageClass: 'alert-danger'
+        });
+    }
+});
+
+router.get('/createAccount', (req, res) => {
+    res.render('createAccount');
+});
+
+router.post('/createAccount', (req, res) => {
+    const { email, firstName, lastName, password, confirmPasword } = req.body;
+
+    // Check if the password and confirm password fields match
+    if (password === confirmPasword) {
+
+        //Check if user with the same email is registered
+        if (users.find(user => user.email === email)) {
+
+            res.render('createAccount', {
+                message: 'User already created.',
+                messageClass: 'alert-danger'
+            });
+
+            return;
+        } const hashedPassword = getHashedPassword(password);
+
+        //Store user into database
+        users.push({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword
+        });
+
+        res.render("index", {
+            message: "Registration Complete. Continue to login please.",
+            messageClass: "alert-success"
+        });
+    } else {
+        res.render('createAccount', {
+            message: 'Password is not a match',
+            messageClass: 'alert-danger'
+        });
+    }
+});
+
+router.get('/protected', (req, res) => {
+    if (req.user) {
+        res.render('/protected');
+    } else {
+        res.render('/index', {
+            message: 'Please login to continue',
+            messageClass: 'alert-danger'
+        });
+    }
+});

c4ee3725eb147c755b8333b2eefbd1ada7045ab9
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-03-15 18:29:27 +1100

    mysql images and seed file

--- db/seed.js
+++ db/seed.js
@@ -1,8 +1,6 @@
 const fs = require("fs");
 const db = require("../models");
 
-db.Customer.findAll({}).then(result => console.log(result));
-
 const customers = [
   {
     first_name: "mushtaq",
@@ -46,42 +44,77 @@ const customers = [
   }
 ];
 
-const file = fs.readFile("../media/img/product.JPG", result => {
-  return result;
-});
-
-console.log(file);
-
-const products = [
-  {
-    product_name: "First product",
-    product_description: "Description of the product",
-    inventory: 100,
-    purchase_price: 15.22,
-    selling_price: 49.99
-  },
-  {
-    product_name: "Second product",
-    product_description: "Description of the product",
-    inventory: 100,
-    purchase_price: 15.22,
-    selling_price: 49.99
-  },
-  {
-    product_name: "Third product",
-    product_description: "Description of the product",
-    inventory: 100,
-    purchase_price: 15.22,
-    selling_price: 49.99
-  }
-];
-
 const orders = [
   {
     quantity: 1,
     order_status: "cart-item",
     CustomerId: 1,
     ProductId: 1
+  },
+  {
+    quantity: 1,
+    order_status: "cart-item",
+    CustomerId: 2,
+    ProductId: 2
+  },
+  {
+    quantity: 1,
+    order_status: "cart-item",
+    CustomerId: 3,
+    ProductId: 3
+  },
+  {
+    quantity: 1,
+    order_status: "cart-item",
+    CustomerId: 4,
+    ProductId: 4
+  }
+];
+
+
+function readImageFile(file) {
+  // read binary data from a file:
+  const bitmap = fs.readFileSync(file);
+  const result = new Buffer(bitmap);
+  return result;
+}
+
+const products = [
+  {
+    product_name: "PUMA Women's Carina Sneaker",
+    product_description: "Description of the product",
+    inventory: 100,
+    purchase_price: 15.22,
+    selling_price: 49.99,
+    product_image: readImageFile("../public/img/1.JPG"),
+    product_url: "/img/1.JPG"
+  },
+  {
+    product_name: "adidas Men's X9000l2 Trail Running Shoe",
+    product_description: "Description of the product",
+    inventory: 100,
+    purchase_price: 15.22,
+    selling_price: 49.99,
+    product_image: readImageFile("../public/img/2.JPG"),
+    product_url: "/img/2.JPG"
+  },
+  {
+    product_name: "adidas Women's Questar Flow Running Shoe",
+    product_description: "Description of the product",
+    inventory: 100,
+    purchase_price: 15.22,
+    selling_price: 49.99,
+    product_image: readImageFile("../public/img/3.JPG"),
+    product_url: "/img/3.JPG"
+  },
+  {
+    product_name: "adidas Women's Questar Flow Shoe",
+    product_description: "Description of the product",
+    inventory: 100,
+    purchase_price: 15.22,
+    selling_price: 49.99,
+    product_image: readImageFile("../public/img/4.JPG"),
+    product_url: "/img/4.JPG"
   }
 ];
 

075fbba78652b8a6ac2d88a12eecf3e25f49396d
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-03-19 10:16:05 +1100

    add biz cols

--- public/js/create.js
+++ public/js/create.js
@@ -1,44 +1,51 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 document.addEventListener("DOMContentLoaded", event => {
   if (event) {
-    console.info("DOM loaded 🚀");
+    console.info("Create DOM loaded 🚀");
   }
 
   // Form references
   const fnInput = document.getElementById("first-name");
   const lnInput = document.getElementById("last-name");
-  const phoneInput = document.getElementById("phone-number");
   const emailInput = document.getElementById("email-address");
   const passwordInput = document.getElementById("passwordInput");
   const confirmPasswordInput = document.getElementById("confirmPasswordInput");
+  const businessInput = document.getElementById("business-name");
+  const phoneInput = document.getElementById("phone-number");
   const createForm = document.getElementById("create-form");
 
   // Event handler for when form is submitted
   const handleFormSubmit = e => {
     e.preventDefault();
 
-    // Make sure the form isn"t empty
+    // Make sure the form isn't empty
     if (
       !fnInput.value.trim() ||
       !lnInput.value.trim() ||
-      !phoneInput.value.trim() ||
       !emailInput.value.trim() ||
       !passwordInput.value.trim() ||
       !confirmPasswordInput.value.trim()
     ) {
+      alert("Fields cannot be blank!");
       return;
     }
 
-    // Object that will be sent to the db
     const newAcct = {
       first_name: fnInput.value.trim(),
       last_name: lnInput.value.trim(),
-      phone_number: phoneInput.value.trim(),
       email: emailInput.value.trim(),
-      user_password: passwordInput.value.trim()
+      user_password: passwordInput.value.trim(),
+      business_name: businessInput.value.trim(),
+      phone_number: phoneInput.value.trim()
     };
 
-    createAcct(newAcct);
+    // Make sure passwords match
+    if (passwordInput.value === confirmPasswordInput.value) {
+      createAcct(newAcct);
+    } else {
+      alert("Passwords do not match!");
+      return;
+    }
   };
 
   createForm.addEventListener("submit", handleFormSubmit);

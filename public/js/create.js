423de26567a6ac9e8f747210efd9a451f905d647
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-19 16:46:25 +1100

    submit working with alert- need to fix if statement

--- public/js/create.js
+++ public/js/create.js
@@ -13,43 +13,48 @@ document.addEventListener("DOMContentLoaded", event => {
 
   // Event handler for when form is submitted
   //const handleFormSubmit = e => {
-    document.getElementById("login-form").addEventListener("submit", e => {
-  e.preventDefault();
-  const userData = {
-    first_name: fnInput.value.trim(),
-    last_name: lnInput.value.trim(),
-    email: emailInput.value.trim(),
-    user_password: passwordInput.value.trim(),
-    confirmPassword: confirmPasswordInput.value.trim()
-  };
-  if (!userData.fnInput || !userData.lnInput.value.trim() || !userData.emailInput.value.trim() || !userData.passwordInput.value.trim() || !userData.confirmPasswordInput.value.trim()
-  ) {
-    alert("Fields cannot be blank!");
-    return;
-  }
-}
-    console.log("something");
-    fetch("/api/createAccount", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        email: emailInput,
-        password: passwordInput
-      })
-    })
-      .then(res => res.json())
-      .then(data => {
-        console.log(data);
-        if (passwordInput.value === confirmPasswordInput.value) {
-          createAcct(newAcct);
-          window.location.href = "/login";
-        } else {
-          alert("Passwords do not match!");
-          window.location.href = "/createAccount";
-          return;
-        }
-      });
+  document.getElementById("create-form").addEventListener("submit", e => {
+    e.preventDefault();
+    const userData = {
+      first_name: fnInput.value.trim(),
+      last_name: lnInput.value.trim(),
+      email: emailInput.value.trim(),
+      user_password: passwordInput.value.trim(),
+      confirmPassword: confirmPasswordInput.value.trim()
+    };
+    if (
+      !userData.fnInput ||
+      !userData.lnInput.value.trim() ||
+      !userData.emailInput.value.trim() ||
+      !userData.passwordInput.value.trim() ||
+      !userData.confirmPasswordInput.value.trim()
+    ) {
+      alert("Fields cannot be blank!");
+      return;
+    }
   });
+  console.log("something");
+  fetch("/api/createAccount", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email: emailInput,
+      password: passwordInput
+    })
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if (passwordInput.value === confirmPasswordInput.value) {
+        createAcct(newAcct);
+        window.location.href = "/login";
+      } else {
+        alert("Passwords do not match!");
+        window.location.href = "/createAccount";
+        return;
+      }
+    });
+});

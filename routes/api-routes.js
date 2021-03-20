0d956fccf19a3a73bfd406e620910837a196705b
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-20 12:53:48 +1100

    adding C for customer hoping to fix

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -128,7 +128,7 @@ router.post("/api/createAccount", (req, res) => {
   //Check if the password and confirm password fields match
   if (password === confirmPassword) {
     //     //Check if user with the same email is registered
-    if (db.customers.find(customer => customer.email === email)) {
+    if (db.Customer.find(customer => customer.email === email)) {
       res.render("createAccount", {
         message: "User already created.",
         messageClass: "alert-danger"

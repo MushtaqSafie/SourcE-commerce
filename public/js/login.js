cd69c692b8ea39176b956e10475f5b60298f33c9
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-03-20 15:09:09 +1100

    add structure to retrieve display client name in sidebar

--- public/js/login.js
+++ public/js/login.js
@@ -1,44 +1,57 @@
-/* eslint-disable prettier/prettier */
-document.getElementById("login-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  const emailInput = document.getElementById("email-address").value.trim();
-  const passwordInput = document.getElementById("passwordInput").value.trim();
+document.addEventListener("DOMContentLoaded", (event) => {
+  if (event) {
+    console.info("Login DOM loaded 🚀");
+  }
 
-  fetch("/api/index", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: emailInput,
-      password: passwordInput,
-    }),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      if (data.isValid) {
-        // checking for client-type if its customer OR business-owner
-        if (data.client_type === "business-owner") {
-          // render salesDash
-          window.location.href = "/salesDash";
-          console.log("client is a business owner");
-          // display the first and last name of the user on sidebar
+  /* eslint-disable prettier/prettier */
+  document.getElementById("login-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const emailInput = document.getElementById("email-address").value.trim();
+    const passwordInput = document.getElementById("passwordInput").value.trim();
+
+    fetch("/api/index", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: emailInput,
+        password: passwordInput,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const userData = data.first_name + " " + data.last_name;
+        const user = userData.toString();
+
+        if (data.isValid) {
+          // checking for client-type if its customer OR business-owner
+          if (data.client_type === "business-owner") {
+            // render salesDash
+            // window.location.href = "/salesDash";
+            console.log("client is a business owner");
+            // display the first and last name of the user on sidebar
+            console.log(user);
+            document.getElementById("loginUser").appendChild(user);
+
+            // return user;
+          } else {
+            // client type is "customer" render StoreFront
+            // window.location.href = "/storeFront";
+            console.log("client is a customer");
+            // display the first and last name of the user on sidebar
+            // document.getElementById("loginUser").appendChild(user);
+          }
         } else {
-          // client type is "customer" render StoreFront
-          window.location.href = "/storeFront";
-          console.log("client is a customer");
-          // display the first and last name of the user on sidebar
+          // checking if client-type is assigned as wrongPass OR notFound
+          if (data.client_type === "wrongPass") {
+            alert("Wrong password, please try again!");
+          } else {
+            alert("User not found. Please try again or create an account.");
+          }
         }
-      } else {
-        // checking if client-type is assigned as wrongPass OR notFound
-        if (data.client_type === "wrongPass") {
-          alert("Wrong password, please try again!");
-        } else {
-          console.log("client is not found");
-          alert("User not found. Please try again or create an account.");
-        }
-      }
-    });
+      });
+  });
 });

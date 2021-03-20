ea2019f91683e862787579bb21b16e1a16dd4873
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-03-20 12:52:27 +1100

    adding changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const db = require("../models/customers");
+const db = require("../models/customer");
 
 const router = express.Router();
 

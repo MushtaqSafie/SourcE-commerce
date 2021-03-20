f26b3ad19135fd18ebb2d6692bc7b34e79fd8a0c
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-03-20 13:21:47 +1100

    add error msgs

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-document.getElementById("login-form").addEventListener("submit", e => {
+document.getElementById("login-form").addEventListener("submit", (e) => {
   e.preventDefault();
   const emailInput = document.getElementById("email-address").value.trim();
   const passwordInput = document.getElementById("passwordInput").value.trim();
@@ -8,41 +8,36 @@ document.getElementById("login-form").addEventListener("submit", e => {
     method: "POST",
     headers: {
       Accept: "application/json",
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       email: emailInput,
-      password: passwordInput
-    })
+      password: passwordInput,
+    }),
   })
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       console.log(data);
       if (data.isValid) {
         // checking for client-type if its customer OR business-owner
         if (data.client_type === "business-owner") {
-          // render salesDash 
+          // render salesDash
           window.location.href = "/salesDash";
           console.log("client is a business owner");
-          // display the first and lost name of the user on sidebar 
-
+          // display the first and last name of the user on sidebar
         } else {
           // client type is "customer" render StoreFront
           window.location.href = "/storeFront";
           console.log("client is a customer");
-          // display the first and lost name of the user on sidebar 
-
+          // display the first and last name of the user on sidebar
         }
       } else {
         // checking if client-type is assigned as wrongPass OR notFound
         if (data.client_type === "wrongPass") {
-          console.log("client is valid but typed worng password");
-          // display error msg that pass is worng
-
+          alert("Wrong password, please try again!");
         } else {
           console.log("client is not found");
-          // display a msg that user is not found. type a correct email&pass OR create new account
-          
+          alert("User not found. Please try again or create an account.");
         }
       }
     });

ff5b6a8f941c9898fd0c2907a9af14794995e3f4
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-03-22 16:55:43 +1100

    fix connect data

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql2 = require("mysql2");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const htmlRouter = require("./routes/html-routes.js");
@@ -11,9 +12,9 @@ const authTokens = {};
 const PORT = process.env.PORT || 8080;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql2.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+  connection = mysql2.createConnection({
     host: "localhost",
     user: "root",
     password: "yourRootPassword",
